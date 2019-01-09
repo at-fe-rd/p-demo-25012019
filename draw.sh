bdfbbe1114602fcbcdbb509275dd78b961923728
at-vinguyen <vi.nguyen@asiantech.vn>
2019-01-09 10:42:01 +0700

    adding script to deploy

---
+++ draw.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+set -e -o pipefail
+# set -e pipefail
+
+set -e
+  reset="\033[0m"
+  red="\033[31m"
+  green="\033[32m"
+  yellow="\033[33m"
+  cyan="\033[36m"
+  white="\033[37m"
+  BUILD_ENV=""
+  ENV_NAME=""
+  password=""
+  version=""
+  SUDO=""
+  PUBLIC_HTML="/var/www/imsv2.asiantech.vn/public_html/"
+
+shopt -s extglob
+printf "${cyan}tttttttttttttttttttttttttttttttttttttttttttt..............................................\n"
+printf "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa..............................................\n"
+printf "aaaaaaaaaaaaaaaaaa         aaaaaaaaaaaaaaaaa..............................................\n"
+printf "aaaaaaaaaaaaaaaa    aaaaaa   aaaaaaaaaaaaaaa..............................................\n"
+printf "aaaaaaaaaaaaaaaa   aaaaaaa   aaaaaaaaaaaaaaa........                      ................\n"
+printf "aaaaaaaaaaaaaaaa    aaaaaa   aaaaaaaaaaaaaaa........  ffffffff . eeeeeee  ................\n"
+printf "aaaaaaaaaaaaaaaaaa    aaa    aaa aaaaaaaaaaa........  fff     .. eee      ................\n"
+printf "aaaaaaaaaaaaaaaaaaaaaaaaaaaa  a   tttttttttt........  fffffff  . eeeeeee  ................\n"
+printf "ttttttttttttttttttttttttttttttt   tttttttttt........  fff   .... eee      ................\n"
+printf "tttttttttttttttttttttttttttttt        tttttt........  fff   .... eeeeeee  ................\n"
+printf "ttttttttttttttttttttttttttttttt   tttttttttt........                      ................\n"
+printf "ttttttttttttttttttttttttttttttt   ttt  ttttt..............................................\n"
+printf "ttttttttttttttttttttttttttttttttt   tttttttt..............................................\n"
+printf "tttttttttttttttttttttttttttttttttttttttttttt..............................................\n"
+printf "tttttttttttttttttttttttttttttttttttttttttttt..............................................\n"
+printf "tttttttttttttttttttttttttttttt${white}Make Your Job More Enjoyable${cyan}.................................\n"
+printf "tttttttttttttttttttttttttttttttttttttttttttt..............................................\n"
+printf "tttttttttttttttttttttttttttttttttttttttttttt..............................................\n"
+printf "${reset}                                                                                  \n"
+
---
+++ src/app/utils/form-validation.ts
@@ -0,0 +1,28 @@
+import { any } from "prop-types";
+
+2
+3
+4
+5
+6
+7
+/**
+ * Validates whether a field has a value
+ * @param {IValues} values - All the field values in the form
+ * @param {string} fieldName - The field to validate
+ * @returns {string} - The error message
+ */
+export const required = (values: any, fieldName: string): string =>
+  values[fieldName] === undefined ||
+  values[fieldName] === null ||
+  values[fieldName] === ""
+    ? "This must be populated"
+    : "";
+
+export class FormValidation {
+  form: any;
+  constructor() {
+    //
+  }
+  
+}
