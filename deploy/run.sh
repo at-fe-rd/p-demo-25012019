dfbace11427a0f140ff871358e014583906993b6
at-vinguyen <vi.nguyen@asiantech.vn>
2019-01-10 14:37:00 +0700

    fix dir to deploy

--- deploy/run.sh
+++ deploy/run.sh
@@ -84,7 +84,7 @@ deploy() {
   sudo -S rm -rf $PUBLIC_HTML*
 
   printf "$cyan> Moving new app to root folder...\n"
-  sudo -S mv $SOURCE_DIR/build/* $PUBLIC_HTML
+  sudo -S mv ~/$SOURCE_DIR/build/* $PUBLIC_HTML
 
   if [ "$2" = "--maintenance" ]; then
     printf "$red> Server $ENV_NAME has suspended!$reset\n"
@@ -106,7 +106,7 @@ execute_build() {
     env_suggestion
   fi
 
-  printf "tttttttttttttttttttttttttttttttttttttttttttt..............................................\n"
+  printf "${cyan}tttttttttttttttttttttttttttttttttttttttttttt..............................................\n"
   printf "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa..............................................\n"
   printf "aaaaaaaaaaaaaaaaaa         aaaaaaaaaaaaaaaaa..............................................\n"
   printf "aaaaaaaaaaaaaaaa    aaaaaa   aaaaaaaaaaaaaaa..............................................\n"
