474ff14b5a57ad98c4dde98a15c09ad888f1a7c6
at-vinguyen <vi.nguyen@asiantech.vn>
2019-01-16 16:46:04 +0700

    correct folder app/container

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router';
-import { App as MainApp } from 'app/containers/App/character';
+import { App as MainApp } from 'app/containers/app/character';
 import { hot } from 'react-hot-loader';
 
 export const App = hot(module)(() => (
