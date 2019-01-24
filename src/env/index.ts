f18d14523280d0032c95949343f4c7f1a193b7ee
at-vinguyen <vi.nguyen@asiantech.vn>
2019-01-24 15:54:48 +0700

    updating comment for env

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,7 +1,6 @@
 // The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
+// The build system defaults to the dev environment which uses `index.ts`, but if you do
+// `npm run build.prod` then `prod.ts` will be used instead.
 export const environment = {
   production: false,
   baseURL: 'http://172.16.110.189/api'
